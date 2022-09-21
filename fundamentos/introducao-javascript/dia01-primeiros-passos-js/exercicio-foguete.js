e6b4e094ad06d2683cab4c9059f3842bdd3fda98
Lucas Echebeste <echebesteee.lucas@gmail.com>
2022-09-21 15:55:49 -0300

    segundo exercicio com foguetinhos

--- fundamentos/introducao-javascript/dia01-primeiros-passos-js/exercicio-foguete.js
+++ fundamentos/introducao-javascript/dia01-primeiros-passos-js/exercicio-foguete.js
@@ -22,9 +22,29 @@ birthYear = 2030
 
 console.log(birthYear)
 
-birthCity = "Porto Alegre"
+//birthCity = "Porto Alegre"
 
 console.log(birthCity)
 
 /* A mensagem de erro é pela razão de que não podemos alterar uma constante. */ 
+
+
+
+
+
+/*Crie uma constante chamada base e atribua a ela o valor 5.
+Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo. */
+
+const base = 5
+const heigth = 8
+const area = base * heigth 
+
+console.log(area)
+
+const perimeter = base + base + heigth + heigth
+
+console.log(perimeter)
+
 
