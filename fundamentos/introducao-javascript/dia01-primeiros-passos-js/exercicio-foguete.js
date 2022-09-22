01d679b6d3ca36f0463b0df613ed9e90246005d5
Lucas Echebeste <echebesteee.lucas@gmail.com>
2022-09-22 14:26:47 -0300

    exercicio resolvido

--- fundamentos/introducao-javascript/dia01-primeiros-passos-js/exercicio-foguete.js
+++ fundamentos/introducao-javascript/dia01-primeiros-passos-js/exercicio-foguete.js
@@ -77,3 +77,41 @@ if (nota >= 80) {
 
 
 
+
+/*Crie uma variável chamada “weekDay” que receba a string “quarta-feira”.
+
+Utilizando if/else, implemente condicionais para que:
+
+Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
+
+Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.
+
+Experimente trocar o valor da string ou até mesmo montar seu próprio algoritmo. Esse assunto é muito importante para seu aprendizado :). */
+
+let weekDay = "segunda-feira"
+
+if ( weekDay === "segunda-feira" ) {
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+
+} else if (weekDay === "terça-feira") {
+    
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+
+} else if (weekDay === "quarta-feira") {
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+
+} else if (weekDay === "quinta-feira") {
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+
+} else if (weekDay === "sexta-feira") {
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+    
+} else {
+
+    console.log("FINALMENTE, descanso merecido UwU")
+
+}
