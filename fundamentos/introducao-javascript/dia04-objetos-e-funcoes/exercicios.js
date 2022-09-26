6ea3d4c5d2a3a521ce656e0240a32f7500d8cfd1
Lucas Echebeste <echebesteee.lucas@gmail.com>
2022-09-26 15:50:00 -0300

    resolvendo exercicios do dia

---
+++ fundamentos/introducao-javascript/dia04-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,64 @@
+
+/*
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log("Bem vinda," , info.personagem);
+  */
+
+  
+  /*
+  let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info['recorrente'] = 'Sim'
+
+  //console.log(info);
+
+
+  for (let propriedades in info) {
+    console.log(propriedades);
+  }
+
+
+
+  for (let propriedades in info) {
+    console.log(info[propriedades]);
+  }
+
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+  for (let propriedades in info) {
+
+  }
+
+  
+ for (propriedades in info ) {}
+
+if (propriedades === 'recorrente' &&
+ 
+info['recorrente'] === 'Sim' && 
+
+info2['recorrente'] === 'Sim') {
+
+    console.log('Ambos recorrentes'); 
+
+} else {
+
+    console.log(info[propriedades] + ' e ' + info2[propriedades]);
+}
+
+*/
+
