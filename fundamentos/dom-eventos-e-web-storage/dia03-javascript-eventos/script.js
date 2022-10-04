0481067a2fb9abfece5cf5d2e18d65116082dd14
Lucas Echebeste <echebesteee.lucas@gmail.com>
2022-10-03 21:45:10 -0300

    exercicios dia 5.3

--- fundamentos/dom-eventos-e-web-storage/dia03-javascript-eventos/script.js
+++ fundamentos/dom-eventos-e-web-storage/dia03-javascript-eventos/script.js
@@ -1,23 +1,4 @@
-function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  }
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  
-  //Exercício Parte 1 - Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione dentro da tag <ul> com id "days".
-
- /* let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function createDaysOfTheMonth() {
   let getDaysList = document.querySelector('#days');
@@ -48,17 +29,142 @@ function createDaysOfTheMonth() {
   }
 }
 
-createDaysOfTheMonth();
+function createDaysOfTheMonth() {
+  
+  }
+  createDaysOfTheMonth();
 
-*/
 
-const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-function criaDiasDoMes () {
-    let minhaLista = document.querySelector('#days');
-    for (let i = 0; i < decemberDaysList.length ; i += 1) {
-        let dias = decemberDaysList[i];
-        let minhaLi = document.createElement('li');
-        minhaLi.ele
+  //Exercicio 2
+
+  function createHolidayButton(buttonName) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-holiday';
+  
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    
+    buttonContainer.appendChild(newButton);
+  }
+  
+  createHolidayButton('Feriados');
+  
+  //Exercicio 3
+  function displayHolidays() {
+    let getHolidayButton = document.querySelector('#btn-holiday');
+    let getHolidays = document.querySelectorAll('.holiday')
+    let backgroundColor = 'rgb(238,238,238)';
+    let setNewColor = 'white';
+  
+    getHolidayButton.addEventListener('click', function() {
+      for (let index = 0; index < getHolidays.length; index += 1) {
+        if (getHolidays[index].style.backgroundColor === setNewColor) {
+          getHolidays[index].style.backgroundColor = backgroundColor;
+        } else {
+          getHolidays[index].style.backgroundColor = setNewColor;
+        }
+      }
+    });
+  }
+
+  function displayHolidays() {
+    
     }
+    
+    displayHolidays();
+
+
+  // Exercicio 4
+
+  function createFridayButton(buttonName) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-friday';
+  
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    buttonContainer.appendChild(newButton); //adiciona o botão como filho do container de botões
+  }
+
+  function createFridayButton(buttonName) {
+    
+    }
+    
+    createFridayButton('Sexta-feira');
+
+
+// Exercicio 5 
+
+function displayFridays(fridaysArray) {
+  let getFridayButton = document.querySelector('#btn-friday');
+  let fridays = document.getElementsByClassName('friday');
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', function() {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+        //caso o texto não tenha sido substituído, ao clicar no botão ele será substituido pelo novo texto;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+        //caso o texto já tenha sido substituído, ao clicar no botão ele volta ao texto padrão.
+      }
+    }
+  });
 }
+
+function displayFridays(fridaysArray) {
+
+  }
+  
+  let decemberFridays = [ 4, 11, 18, 25 ];
+  displayFridays(decemberFridays);
+
+
+//Exercicio 6
+
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600'; // Ele pega o evento alvo e altera o estilo de fontWeight para 600
+  });
+}
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontSize = '20px';
+    event.target.style.fontWeight = '200'; // Ele pega o evento alvo e altera o estilo de fontWeight para 200
+  });
+}
+
+function dayMouseOver() {
+  
+}
+
+function dayMouseOut() {
+  
+}
+dayMouseOver();
+dayMouseOut();
+
+
+//Exercicio 7
+
+function newTaskSpan(task) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  tasksContainer.appendChild(taskName); // Anexando a task como filha de taskContainer
+}
+
+function newTaskSpan(task) {
+  
+}
+
+newTaskSpan('projeto');
