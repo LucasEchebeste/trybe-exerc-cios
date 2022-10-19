c23d8c5afcd8f67d293979fd6c2ba192da543c20
Lucas Echebeste <echebesteee.lucas@gmail.com>
2022-10-18 23:39:01 -0300

    exercicios seção 7 dia 2

---
+++ fundamentos/introducao-javascript-es6-testes-unitarios/dia02-fluxo-exceção-e-objetos/script.js
@@ -0,0 +1,108 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      },
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  };
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  };
+  
+  orderModifier(order);const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+
+  
+const orderModifier = (order) => {
+    const newBuyer = order.name = 'Luiz Silva'; // Altere o nome da pessoa compradora
+    const newTotal = order.payment.total = '50'; // Altere o valor total da compra
+    const pizzas = Object.keys(order.order.pizza); // Recupere as chaves que estão em `order.order.pizza`
+    const drinks = order.order.drinks.coke.type; // Recupere o `type` da chave `coke`.
+
+    console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${newTotal},00.`); // Exiba a mensagem desejada utilizando `template literals`
+  }
+  
+  orderModifier(order);
+
+  const verifyPair = (obj, key, value) => {
+    const arr = Object.entries(obj);
+    let isEqual = false;
+    for (let index in arr) {
+      if (arr[index][0] === key && arr[index][1] === value) isEqual = true;
+    }
+    return isEqual;
+  };
+  console.log(verifyPair(lesson2,'professor','Carlos'));
+
+
